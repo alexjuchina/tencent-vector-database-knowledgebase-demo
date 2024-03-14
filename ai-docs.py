a304d75421bdad415a9d037ed9e6e8effbc6d049
Alex Ju <18329894+alexjuchina@users.noreply.github.com>
2024-03-14 21:54:58 +0800

    去掉百川，增加 openai

--- ai-docs.py
+++ ai-docs.py
@@ -1,144 +1,124 @@
 from flask import Flask, request, jsonify
 import json
-import hmac
-import hashlib
-import base64
-import time
 import requests
-import json
-import tcvectordb
-import os
-import tcvectordb
-from tcvectordb import exceptions
-from tcvectordb.model.collection_view import Embedding
+from tcvectordb import VectorDBClient
 from zhipuai import ZhipuAI
+import random
+from http import HTTPStatus
 
-
-vdbclient = tcvectordb.VectorDBClient(
-    url='http://lb-n27j42ga-xxx.clb.ap-guangzhou.tencentclb.com:40000', 
+vdbclient = VectorDBClient(
+    url='http://lb-xxxxxxxx.clb.ap-guangzhou.tencentclb.com:40000', 
     username='root', 
-    key='xxx'
+    key='xxxxx'
     )
 
 app = Flask(__name__)
-access_token = "xxx" #TEST
+access_token = "xxxx"  # 请替换为您的具体access_token
 DINGDING_WEBHOOK_URL = f"https://oapi.dingtalk.com/robot/send?access_token={access_token}"
 PORT = 8890
- 
-def searchKnowlege(question):
+prompt = "你是一个gitlab专家，可以根据用户给的背景知识回答问题。如果背景知识里面没有找到答案，直接说找不到答案。"
+
+def search_knowledge(question):
     db = vdbclient.database('testdb')
     collView = db.collection_view('knowlege')
     doc_list = collView.search(
             content=question,
             limit=5,
         )
-    knowlege = ''
-    print("\n\n查询向量数据库：")
-    for count,doc in enumerate(doc_list):
-            print(f"===================== 查询到知识条目 {count}=====================")
-            print(doc.data.text)
-            knowlege += doc.data.text
-    return knowlege
+    knowledge = ''
+    for count, doc in enumerate(doc_list):
+        knowledge += doc.data.text
+    return knowledge
 
-# baichuan
-def generate_answer_from_baichuan(msg):
-    url = "https://api.baichuan-ai.com/v1/chat/completions"
-    api_key = "sk-xxxxxx"
-    data = {
-        "model": "Baichuan2-Turbo",
-        "messages": [{
-                "role": "user",
-                "content": msg
-            }]
-    }
-    json_data = json.dumps(data)
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + api_key
-    }
-    response = requests.post(url, data=json_data, headers=headers,timeout=60)
-    if response.status_code == 200:
-        reply = json.loads(response.text)["choices"][0]["message"]["content"]
-        return reply
-    else:
-        print(response.text)
-        print("请求失败，状态码:", response.status_code)
+def generate_answer_openai(msg):
+    # 请替换为您的OpenAI API密钥
+    openai.api_key = "YOUR_API_KEY"
+    messages = [
+        {"role": "system", "content": prompt},
+        {"role": "user", "content": msg},
+    ]
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    response_content = response.choices[0].message.content.replace('\n\n', '\n')
+    return response_content
 
-# glm4
-def generate_answer_from_zhipu(msg):
-    client = ZhipuAI(api_key="xxx")
+def generate_answer_qianwen(msg):
+    # 请替换为您的dashscope API密钥
+    messages = [{'role': 'system', 'content': prompt},
+                {'role': 'user', 'content': msg}]
+    response = dashscope.Generation.call(
+        dashscope.Generation.Models.qwen_plus,
+        messages=messages,
+        seed=random.randint(1, 10000),
+        result_format='message',
+    )
+    content = response['output']['choices'][0]['message']['content']
+    return content
+
+def generate_answer_zhipu(msg):
+    client = ZhipuAI(api_key="YOUR_API_KEY")
     response = client.chat.completions.create(
         model="glm-4",
-        messages=[
-            {
-                "role": "user",
-                "content": msg
-            },
-            {
-                "role": "system", 
-                "content": "你是一个 GitLab 专家，你的任务是根据用户给出的问题和背景知识，给出专业的回答，回答内容中如果涉及 markdown 的标题和代码块，请去掉'#'井号以及'```'这样的代码块标识，如果你根据背景知识没有找到合适的答案，不要编造答案。"
-            }
-        ],
+        messages=[{"role": "user", "content": msg}],
         top_p=0.7,
         temperature=0.95,
         max_tokens=1024,
-        stream=True,
+        stream=False,
     )
-    answer = ''
-    for chunk in response:
-        answer += chunk.choices[0].delta.content
+    content = json.loads(response.json())['choices'][0]['message']['content']
+    return content
 
-    return answer
-    
-# send dingdig message 
 def send_to_dingding(dingding_msg):
     url = f"https://oapi.dingtalk.com/robot/send?access_token={access_token}"
     headers = {'Content-Type': 'application/json;charset=utf-8'}
     data = {
         "msgtype": "markdown",
         "markdown": {
-            "title":"AI Answer",
+            "title": "AI Answer",
             "text": dingding_msg
-            }
+        }
     }
-    print(dingding_msg)
     response = requests.post(url, headers=headers, data=json.dumps(data))
-    if response.status_code == "0":
-        print("\n钉钉消息发送成功：\n", dingding_msg)
+    errcode = response.json()["errcode"]
+    if errcode == 0:
+        print("钉钉消息发送成功：", dingding_msg)
     else:
-        print("\n钉钉消息发送失败并提醒用户：\n", response.json())
+        print("钉钉消息发送失败并提醒用户：", str(response.json()))
         dingding_msg = str(response.json())
         data = {
-        "msgtype": "markdown",
-        "markdown": {
-            "title":"AI Answer",
-            "text": dingding_msg
+            "msgtype": "markdown",
+            "markdown": {
+                "title": "AI Answer",
+                "text": "出错了：" + dingding_msg
             }
         }
         response = requests.post(url, headers=headers, data=json.dumps(data))
 
 @app.route('/dingding', methods=['POST'])
 def dingding():
-   data = request.json
-   question = data.get('text', {}).get('content')
-   print("\n钉钉问题:\n", question)
-
-   knowleges = searchKnowlege(question)
-   content = json.dumps({
+    data = request.json
+    question = data.get('text', {}).get('content')
+    knowledges = search_knowledge(question)
+    content = json.dumps({
          "请回答问题：": question,
-         "背景知识如下：": knowleges
-      },ensure_ascii=False)
-   answer_baichuan=generate_answer_from_baichuan(content)
-   answer_zhipu=generate_answer_from_zhipu(content)
-    # 绕开钉钉URL阻断
-   if 'gitlab.com' in answer_zhipu:
+         "背景知识如下：": knowledges
+      }, ensure_ascii=False)
+    answer_openai = generate_answer_openai(content)
+    answer_zhipu = generate_answer_zhipu(content)
+    answer_qianwen = generate_answer_qianwen(content)
+    
+    if 'gitlab.com' in answer_zhipu:
         answer_zhipu = answer_zhipu.replace('gitlab.com', 'example.com')
-   if 'gitlab.com' in answer_zhipu:
-        answer_baichuan = answer_baichuan.replace('gitlab.com', 'example.com')
-   dingding_msg = f"## 百川 AI 回答：\n {answer_baichuan} \n ------------------ \n  ## 智谱 AI 回答：\n {answer_zhipu}"
-   print("\n\n===================== AI 回答：=====================\n\n")
-   send_to_dingding(dingding_msg)
-   return jsonify({"errcode": 0, "errmsg": "success"})
+    if 'gitlab.com' in answer_openai:
+        answer_openai = answer_openai.replace('gitlab.com', 'example.com')
+    if 'gitlab.com' in answer_qianwen:
+        answer_qianwen = answer_qianwen.replace('gitlab.com', 'example.com')
+        
+    dingding_msg = f"# Openai：\n{answer_openai}\n<br> <br> \n# ChatGLM4：\n{answer_zhipu}\n<br> <br> \n# 通义千问：\n{answer_qianwen}"
+    send_to_dingding(dingding_msg)
+    return jsonify({"errcode": 0, "errmsg": "success"})
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run(host='0.0.0.0', port=PORT, debug=False)
